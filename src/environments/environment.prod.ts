46dc1c73222befdddfd535bf31b74cf96ece5dc5
Sudhir Tiwari <Sudhir.Tiwari@Clarivate.com>
2026-04-16 12:31:52 +0530

    added changes for prod V1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  // Replace with your deployed Render backend URL (Step 4)
-  apiBase: 'https://YOUR-BACKEND-NAME.onrender.com/api',
+  apiBase: 'https://portfolio-app-fzof.onrender.com/api',
   googleClientId: '',
   googleApiKey: '',
   gapiDiscoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
